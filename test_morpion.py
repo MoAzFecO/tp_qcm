3233ffc5b168d1e48b09283d5def404b64f5580d
jferon <84545837+MoAzFecO@users.noreply.github.com>
2023-01-06 14:49:43 +0100

    Create test_morpion.py

---
+++ test_morpion.py
@@ -0,0 +1,15 @@
+import pytest
+
+import TicTacToe_2016 as ttt
+
+class TestZone:
+
+    def testtrue(self):
+        assert 1
+
+    def test_create_grid(self):
+        b = ttt.create_grid()
+        print(b)
+        assert b == [[" ", " ", " "],
+                     [" ", " ", " "],
+             [" ", " ", " "]]
